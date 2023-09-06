c1455c417b24a1e36569a9fe6c95877d122bd1a5
Júnior Nunes <denisonprobable13@gmail.com>
2023-09-06 14:40:41 -0300

    feat: create stars component

---
+++ src/components/Stars/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+import TrophySvg from "../../assets/trophy.svg";
+
+export function Stars() {
+  return (
+    <View style={styles.container}>
+      <TrophySvg />
+    </View>
+  );
+}
