f4f9e8fc2ecad60cdc0e8d4a6c5a0b725b22f926
Júnior Nunes <denisonprobable13@gmail.com>
2023-09-06 16:07:21 -0300

    feat: use sounds as feed back

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
+import { Audio } from "expo-av";
 
 import { GestureDetector, Gesture } from "react-native-gesture-handler";
 
@@ -58,6 +59,19 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require(`../../assets/correct.mp3`)
+      : require(`../../assets/wrong.mp3`);
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    if (!sound) return;
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -94,9 +108,11 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await playSound(true);
       setStatusReply(1);
       setPoints((prevState) => prevState + 1);
     } else {
+      await playSound(false);
       setStatusReply(2);
       shakeAnimation();
     }
